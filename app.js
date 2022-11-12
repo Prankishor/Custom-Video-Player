50d35e3b1f72a6015809350bbc6f133973bc0b4b
Prankishor <33565966+Prankishor@users.noreply.github.com>
2022-11-13 00:05:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stopp = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// play & pause video
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play();
+    }
+    else {
+        video.pause();
+    }
+}
+
+//update play/pause icon
+function updatePlayIcon() {
+    if (video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    }
+    else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+    }
+}
+
+//update progress and timestamp
+function updateProgress() {
+    return true;
+}
+
+//stop video
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
+
+//set video time to progress
+function setVideoStatus() {
+    return true;
+}
+
+//Event Listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+play.addEventListener('click', toggleVideoStatus);
+stopp.addEventListener('click', stopVideo);
+progress.addEventListener('change', setVideoStatus);
